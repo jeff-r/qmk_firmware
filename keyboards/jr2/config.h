2aa71bd846485383c32459f10594c86a73cc6831
Jeff <Roush>
2019-07-28 17:18:47 -0700

    Update the key layout for most recent build

--- keyboards/jr2/config.h
+++ keyboards/jr2/config.h
@@ -68,8 +68,17 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // COLS: Left to right, ROWS: Top to bottom
 // Harness 2
 // (Used by 2017.02.1)
+
+
 #define MATRIX_ROW_PINS { F0, F1, F4, F5, F6 }
-#define MATRIX_COL_PINS { D7, D6, D4, D5, C7, C6, D3, D2, D1, D0, B7, E6, B3, B2, B1, B0, B5, B4 }
+
+#define MATRIX_COL_PINS { B0, B1, B2, B3, B7, D0, D1, D2, D3, C6, C7, D5, B4, D4, D6, D7 }
+// #define MATRIX_COL_PINS { B4, D7, D6, D4, D0, D5, C7, C6, B3, B2, B1, B7, B0, B5, E6, D1 }
+// #define MATRIX_COL_PINS { B4, D6, D4, D5, C7, C6, D3, D2, D1, D0, B7, E6, B3, B2, B1, B0, B5, B4 }
+// #define MATRIX_COL_PINS { D7, D6, D4, D5, C7, C6, D3, D2, D1, D0, B7, E6, B3, B2, B1, B0, B5, B4 }
+
+// #define MATRIX_ROW_PINS { F0, F1, F4, F5, F6 }
+// #define MATRIX_COL_PINS { D7, D6, D4, D5, C7, C6, D3, D2, D1, D0, B7, E6, B3, B2, B1, B0, B5, B4 }
 
 // Harness 3
 // (Used by 2017.03.1)
--- keyboards/jr2/keymaps/default/keymap.c
+++ keyboards/jr2/keymaps/default/keymap.c
@@ -17,7 +17,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         {KC_HOME,  KC_PGUP,  KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,     KC_P,      KC_LBRACKET, KC_RBRACKET, F(JR_CTL_GUI)},
         {KC_END,   KC_PGDN,  KC_ESC,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,     KC_SCOLON, KC_QUOTE,    KC_ENTER,    F(JR_CTL_ALT) },
         {KC_UP,    KC_DOWN,  KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLASH,  KC_RSFT,     KC_BSLASH,   KC_BSLASH },
-        {KC_LEFT,  KC_RIGHT, KC_LCTL,  KC_LGUI, KC_LALT, F(JR_LAYER_ONCE),  KC_BSPC,  KC_SPC,  KC_SPC,  F(JR_LAYER_TOGGLE),  KC_RALT, KC_RGUI,  KC_RCTL,   KC_RALT,     KC_RGUI,     KC_RCTL }
+        {KC_LEFT,  KC_RIGHT, KC_LCTL,  KC_LGUI, KC_LALT, F(JR_LAYER_ONCE),  KC_BSPC,  KC_SPC,  KC_SPC,  F(JR_LAYER_TOGGLE),  KC_RALT, KC_LGUI,  KC_RCTL,   KC_RALT,     KC_LGUI,     KC_RCTL }
         // {KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6 },
     },
     [1] = { /* toggle */
